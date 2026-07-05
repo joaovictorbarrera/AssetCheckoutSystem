845f2ca069691a12dd4befc5a137fabf8727644b
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-07-05 14:03:57 -0400

    added default password hash

---
+++ server/Migrations/20260705180313_AddedDefaultPassword.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetCheckoutSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDefaultPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "PasswordHash",
+                value: "0B20E35DE6FF81A819A7190DC4942C816525BDC915D11947D91E34CAA2469D86");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "PasswordHash",
+                value: "0B20E35DE6FF81A819A7190DC4942C816525BDC915D11947D91E34CAA2469D86");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "PasswordHash",
+                value: "0B20E35DE6FF81A819A7190DC4942C816525BDC915D11947D91E34CAA2469D86");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "PasswordHash",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "PasswordHash",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "PasswordHash",
+                value: null);
+        }
+    }
+}
